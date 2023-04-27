5955174b8857fafb53b8a07eb678bdce040847c5
Tanvir Upol <tanvirrahmanupol@gmail.com>
2023-04-27 17:10:10 +0600

    update

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -39,8 +39,8 @@ const Form = () => {
         </label>
         <div className='relative'>
           <select className='block appearance-none w-full  border  text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:border-primaryRed focus:border-2' {...register("role", {required: true})} >
-            <option value="A">A</option>
-            <option value="B">B</option>
+            <option value="sr">sr</option>
+            <option value="dm">dm</option>
           </select>
           <div className="h-full pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
             <svg className="fill-current h-5 w-5 mb-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
