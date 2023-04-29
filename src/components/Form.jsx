bf25ba8a7ae77b16e6398c0e4db087d43b99c6bd
Tanvir Upol <tanvirrahmanupol@gmail.com>
2023-04-29 23:54:47 +0600

    name validation update

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -18,22 +18,27 @@ const Form = () => {
           Name:
         </label>
         <input className='border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-primaryRed focus:border-2' 
-        name='name' type="text" {...register("name", {required: true})} />
-        {errors.name?.type === 'required' && <p>*name is required</p>}
+        name='name' type="text" {...register("name", 
+          { 
+            required: true, 
+            pattern:/^(?![^A-Za-z0-9])([A-Za-z]+(?: [A-Za-z]+)*)?(?<![^A-Za-z0-9])$/
+          })} />
+        {errors.name?.type === 'required' && <p className='text-sm text-red-600' >*name is required</p>}
+        {errors.name?.type === 'pattern' && <p className='text-sm text-red-600' >No special chatacters or numbers</p>}
         
         <label className='mt-3' >
           Phone:
         </label>
         <input className='border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-primaryRed focus:border-2'      name="phone" {...register("phone", {required: true,pattern: /^01[3-9]\d{8}$/
          })}  type="number"/>
-        {errors.phone?.type === 'required' && <p>*phone is required</p>}
-        {errors.phone?.type === 'pattern' && <p>*use correct format</p>}
+        {errors.phone?.type === 'required' && <p className='text-sm text-red-600' >*phone is required</p>}
+        {errors.phone?.type === 'pattern' && <p className='text-sm text-red-600' >*use correct format</p>}
         
         <label className='mt-3' >
           Zone:
         </label>
         <input className='border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-primaryRed focus:border-primaryRed focus:border-2' type="text" {...register("zone", {required: true})} />
-        {errors.zone?.type === 'required' && <p>*zone is required</p>}
+        {errors.zone?.type === 'required' && <p className='text-sm text-red-600' >*zone is required</p>}
         
         <label className='mt-3' >
           Role:
@@ -47,7 +52,7 @@ const Form = () => {
             <svg className="fill-current h-5 w-5 mb-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
           </div>
         </div>
-        {errors.role?.type === 'required' && <p>*role is required</p>}
+        {errors.role?.type === 'required' && <p className='text-sm text-red-600' >*role is required</p>}
       
         
         <label className='mb-4' >
@@ -61,7 +66,7 @@ const Form = () => {
             <input name='status' className='mr-2 accent-primaryRed scale-150' type="radio"  value="offline" {...register("status", {required: true})}  />
             Offline
           </label>
-          {errors.status?.type === 'required' && <p className='*inline-block ml-3' >status is required</p>}
+          {errors.status?.type === 'required' && <p className='*inline-block ml-3 text-sm text-red-600' >status is required</p>}
         </label>
         <button className='bg-primaryRed rounded-md text-white p-2 text-base' type="submit">Submit</button>
       </form>
